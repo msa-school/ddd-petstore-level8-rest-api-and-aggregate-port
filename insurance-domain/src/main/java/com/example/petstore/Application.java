0f2b07151cdf23d2e596cec339293757841287db
Jinyoung Jang <jyjang@uengine.org>
2022-04-07 08:13:38 +0000

    adding insurance domain

--- insurance-domain/src/main/java/com/example/petstore/Application.java
+++ insurance-domain/src/main/java/com/example/petstore/Application.java
@@ -1,9 +1,11 @@
 package com.example.petstore;
 
-import com.example.petstore.kafka.KafkaProcessor;
+import com.example.petstore.domain.Health;
+
+//import com.example.petstore.kafka.KafkaProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
+//import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableAspectJAutoProxy
-@EnableBinding(KafkaProcessor.class)
-public class Application {
+//@EnableBinding(KafkaProcessor.class)
+public class StoreApplication {
 	public static ApplicationContext applicationContext;
 	public static void main(String[] args) {
-		applicationContext = SpringApplication.run(Application.class, args);
+
+		Health health = new Health();
+		health.setBloodPressure(70);
+
+		System.out.println("건강여부 =  "  +   health.checkHealthy());
+
+
+		applicationContext = SpringApplication.run(StoreApplication.class, args);
 	}
 
 
